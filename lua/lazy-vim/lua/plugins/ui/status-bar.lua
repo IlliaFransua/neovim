0dc59e708aa3bdd40b0bf45628cbf6faa843e6f9
harutoman3 <zpkdrubikscube@outlook.jp>
2026-01-22 21:23:17 +0900

    delete branch icon

--- lua/lazy-vim/lua/plugins/ui/status-bar.lua
+++ lua/lazy-vim/lua/plugins/ui/status-bar.lua
@@ -2,6 +2,7 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		opts = function(_, opts)
+			opts.sections.lualine_b = { { "branch", icon = "" } }
 			opts.sections.lualine_x = {}
 			opts.sections.lualine_z = {}
 		end,
