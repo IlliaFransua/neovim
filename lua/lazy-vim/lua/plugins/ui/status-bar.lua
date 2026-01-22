72fc9e9c1ca213431425247c42596a51bf716db3
harutoman3 <zpkdrubikscube@outlook.jp>
2026-01-22 22:51:52 +0900

    cahnge display of modes

--- lua/lazy-vim/lua/plugins/ui/status-bar.lua
+++ lua/lazy-vim/lua/plugins/ui/status-bar.lua
@@ -2,6 +2,27 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		opts = function(_, opts)
+			local mode_icons = {
+				NORMAL = "🏠 ",
+				INSERT = "✍️ ",
+				VISUAL = "👁️ ",
+				["V-LINE"] = "📏 ",
+				["V-BLOCK"] = "🧱 ",
+				COMMAND = "⚙️ ",
+				REPLACE = "🔄 ",
+				TERMINAL = "💻 ",
+				["O-PENDING"] = "⏳ ",
+				SELECT = "🔎 ",
+			}
+
+			opts.sections.lualine_a = {
+				{
+					"mode",
+					fmt = function(res)
+						return (mode_icons[res] or "") .. res
+					end,
+				},
+			}
 			opts.sections.lualine_b = { { "branch", icon = "" } }
 			opts.sections.lualine_x = {}
 			opts.sections.lualine_y = {
